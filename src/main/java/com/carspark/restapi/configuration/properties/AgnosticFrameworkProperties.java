5b5d7057464134fd937ccb8f199a48cd1d1b1dba
Luca Zenobi <luzenobi@gmail.com>
2020-02-20 22:30:04 +0100

    Updated AgnosticFrameworkProperties

--- src/main/java/com/carspark/restapi/configuration/properties/AgnosticFrameworkProperties.java
+++ src/main/java/com/carspark/restapi/configuration/properties/AgnosticFrameworkProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "agnosticframework")
+@ConfigurationProperties(prefix = "agnostic-framework")
 public class AgnosticFrameworkProperties {
 
   private String file = "mappings.json";
